1387cf041cec01e726ca920b749d74d967b394dd
Irena-Pod <irena.podgurski@gmail.com>
2021-02-01 18:41:35 +0000

    implement game

---
+++ main.rb
@@ -0,0 +1,45 @@
+require "./players"
+require "./questions"
+
+# Game setup
+puts "Welcome to TwO-O-Player Math Game, this is a 2-player game!"
+print "Enter player 1 name: "
+name1 = gets.chomp.to_s
+print "Enter player 2 name: "
+name2 = gets.chomp.to_s
+
+player1 = Players.new(name1)
+player2 = Players.new(name2)
+
+count = 0
+
+puts "Welcome #{player1.name} and #{player2.name}, let the game begin.."
+
+while (player1.alive? && player2.alive?) do
+  player = (count == 0) ? player1 : player2
+  puts '----- NEW TURN -----'
+
+  question = Questions.new
+  puts "#{player.name}: #{question.question}"
+  print "> "
+  answer = gets.chomp
+  if answer.to_i == question.answer
+    puts "#{player.name}: YES. YOU ARE CORRECT!"
+    puts "#{player1.name}: #{player1.lives}/3 vs #{player2.name}: #{player2.lives}/3"  
+  else 
+    player.lives -= 1
+    puts "#{player.name} : Seriously? No!"
+    puts "#{player1.name} : #{player1.lives}/3 vs #{player2.name}:  #{player2.lives}/3"
+  end     
+  count = (count + 1) % 2
+end
+
+if player1.lives == 0
+  puts "#{player2.name} wins with a score of #{player2.lives}/3"
+  puts '----- GAME OVER -----'
+  puts 'Good bye!'
+else
+  puts "#{player1.name} wins with a score of #{player1.lives}/3"
+  puts '----- GAME OVER -----'
+  puts 'Good bye!' 
+end 
---
+++ questions.rb
@@ -0,0 +1,11 @@
+class Questions
+  attr_accessor :answer, :question
+
+  def initialize()
+    @number1 = rand(1...20)
+    @number2 = rand(1...20)
+    @answer = @number1 + @number2
+    @question = "What does #{@number1} + #{@number2} qual?"
+  end
+
+end
